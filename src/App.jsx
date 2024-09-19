086247cb533de828c05a4fb95343d61e41e68cb5
Roaa K <ruaakhuldoon.26@gmail.com>
2024-09-20 02:16:55 +0300

    ADD STYLING TO DIE ELEMENTS

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,18 @@ function App() {
 
   return (
     <main>
+      <div className='dice-container'>
        <Die value="1"/>
+       <Die value="1"/>
+       <Die value="1"/>
+       <Die value="1"/>
+       <Die value="1"/>
+       <Die value="1"/>
+       <Die value="1"/>
+       <Die value="1"/>
+       <Die value="1"/>
+       <Die value="1"/>
+       </div>
     </main>
   )
 }
